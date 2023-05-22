4634ffe3e8f12a984dc18ba186b4022850610685
EGOR <staer_mag@mail.ru>
2023-05-22 22:28:09 +0300

    fixed getRandomNum after linter verification

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,2 @@
-const getRandomNum = (min = 1, max = 100) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-};
+const getRandomNum = (min = 1, max = 100) => Math.floor(Math.random() * (max - min)) + min;
 export default getRandomNum;
