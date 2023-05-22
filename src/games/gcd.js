5922c280b54cfbe3927d2235283870ffe22ec697
EGOR <staer_mag@mail.ru>
2023-05-22 16:37:15 +0300

    created brain-gcd

---
+++ src/games/gcd.js
@@ -0,0 +1,28 @@
+import { getRandomNum } from '../utils.js';
+import gamesLogic, { roundsNumber } from '../index.js';
+
+const rule = 'Find the greatest common divisor of given numbers.';
+
+const getGcd = (number1, number2) => {
+  if (number2 === 0) {
+    return number1;
+  }
+  return getGcd(number2, number1 % number2);
+};
+
+const generateResult = () => {
+  const operand1 = getRandomNum();
+  const operand2 = getRandomNum();
+  const question = `${operand1} ${operand2}`;
+  const correctAnswer = String(getGcd(operand1, operand2));
+  return [question, correctAnswer];
+};
+
+const runGcdGame = () => {
+  const results = [];
+  for (let i = 0; i <= roundsNumber - 1; i += 1) {
+    results[i] = generateResult();
+  }
+  return gamesLogic(results, rule);
+};
+export default runGcdGame;
